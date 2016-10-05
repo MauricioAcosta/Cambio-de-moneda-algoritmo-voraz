67a0679510381e65fcb563b50352e9c7918866cc
AndresAcost <andres.taamap@gmail.com>
2016-10-05 01:58:28 -0500

    se implementa logica

--- js/script.js
+++ js/script.js
@@ -7,8 +7,35 @@ function calcular() {
     MatrizMoneda = monedas.split(","); // EN EL VECTOR 'MatrizMoneda' quedan guardadas las monedas
 
     dinero = document.getElementById("Dinero").value
-    alert('Las Moneds para Cambio de: ' + dinero);
+    alert('Las Monedas para Cambio de: ' + dinero+" son "+ MatrizMoneda);
 
+    var cambio = devolverCambio(dinero,MatrizMoneda)
+    var muestra = document.getElementById('Muestra')
+    muestra.innerHTML="\n"+cambio
+
+}
+
+//da el cambio de n unidades utilizando el menor
+//numero posible de monedas. la constante C especifica el
+//numero de monedas disponibles
+function devolverCambio(n,c) {
+//n es conjunto de monedas
+  //var C = [100, 25, 10, 5, 1]
+  var S = new Array() // S es el conjunto que contendra la solucion
+  var s = 0 // s es la suma de los elementos de S
+  while (s != n) {
+    x = Math.max(...c);//El mayor elemento de C
+    if (s + x <= n) { //Se puede cambiar la condicion, pero la dejo asi para que se entienda, puede ponerse negada y tener solo el else
+      x = x
+    } else {
+      //no encuentro la solucion
+      return 'No encuento la solucion'
+    }
+    S.push(x)
+    s = s + x
+  }
+  console.log('S es el conjunto que contendra la solucion'+S);
+  return S
 }
 
 function graficar() {
@@ -34,7 +61,7 @@ function graficar() {
         lapiz.fillText(texto, i * 110, 50);
 
         lapiz.fillStyle = "black"; //color de relleno
-        texto = "Cant= ";
+        texto = "";//Cant=
         lapiz.font = "bold 15px arial";
         lapiz.strokeText(texto, i * 110, 120); //texto con los dos métodos
         lapiz.fillText(texto, i * 110, 120);
